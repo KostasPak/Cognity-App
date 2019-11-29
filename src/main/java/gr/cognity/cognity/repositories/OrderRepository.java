07021036ddb96d7a967d423cf30486763323a652
Kostas <kpakiroglou@cognity.gr>
2019-11-29 15:25:16 +0200

    2019-11-29 15:25

---
+++ src/main/java/gr/cognity/cognity/repositories/OrderRepository.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gr.cognity.cognity.repositories;
+
+import gr.cognity.cognity.models.Customer;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+
+/**
+ *
+ * @author kpakiroglou
+ */
+public interface OrderRepository extends MongoRepository<Customer, String> {
+    
+}
